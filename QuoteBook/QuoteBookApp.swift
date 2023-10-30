c1fb46b2c1efb8c4ee2eaa94870a3c2eb018d09f
Adam <tabievadam@gmail.com>
2023-10-30 18:28:28 +0100

    Initial Commit

---
+++ QuoteBook/QuoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteBookApp.swift
+//  QuoteBook
+//
+//  Created by Адам Табиев on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
