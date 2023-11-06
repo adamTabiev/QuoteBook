db645a4d859026afead514135192280d79417727
Adam <tabievadam@gmail.com>
2023-11-06 13:30:01 +0100

    Minor

--- QuoteBook/Views/HumanCircleView.swift
+++ QuoteBook/Views/HumanCircleView.swift
@@ -8,23 +8,43 @@
 import SwiftUI
 
 struct HumanCircleView: View {
-    let humanImage: String
     
     var body: some View {
-        if let uiImage = UIImage(named: humanImage) {
-            Image(uiImage: uiImage)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 200)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(Color.black.opacity(0.2), lineWidth: 4))
-                .shadow(radius: 10)
-        } else {
-            Text("Изображение не найдено")
+        VStack(spacing: 20) {
+            Image("Платон")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black.opacity(0.2), lineWidth: 4))
+                    .shadow(radius: 10)
+            }
+            
+            Text("Платон")
+                .font(.title)
+                .foregroundColor(Color.blue)
+                .bold()
+            
+            Text("древнегреческий философ 5 век до н.э., ученик Сократа, учитель Аристотеля.")
+                .font(.footnote)
+                .opacity(0.7)
+                .padding(.horizontal)
+            
+            Divider()
+            
+            Text("Наилучшее воспитание молодых людей, да и самих себя, заключается не во внушениях, а в явном для всех осуществлении в собственной жизни того, что внушается другому.")
+                .font(.callout)
+                .multilineTextAlignment(.center)
+                .monospaced()
+            
+            
+                .fontWeight(.bold)
+                .padding(.horizontal)
+            
         }
     }
-}
+
 
 #Preview {
-    HumanCircleView(humanImage: "Платон")
+    HumanCircleView()
 }
