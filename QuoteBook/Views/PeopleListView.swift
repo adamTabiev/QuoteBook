4f6e68b5ed4f8813140811e29d7060d52da3cfb4
Adam <tabievadam@gmail.com>
2023-11-17 12:17:59 +0100

    Stash

---
+++ QuoteBook/Views/PeopleListView.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleListView.swift
+//  QuoteBook
+//
+//  Created by Адам Табиев on 17.11.2023.
+//
+
+import SwiftUI
+
+struct PeopleListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PeopleListView()
+}
