1cc05dfc20c1e7aefa13cb30a8fc11d0849e5896
Adam <tabievadam@gmail.com>
2023-11-23 11:34:23 +0100

    added scrolling animation

--- QuoteBook/Views/CommentView.swift
+++ QuoteBook/Views/CommentView.swift
@@ -23,6 +23,8 @@ struct CommentView: View {
                     .containerRelativeFrame(.horizontal, count: 1, spacing: 1.0)
                     .scrollTransition { content, phase in
                         content
+                            .rotation3DEffect(.degrees(phase.value * -30.0),
+                                              axis: (x: 0, y: 1, z: 0))
                             .opacity(phase.isIdentity ? 1 : 0.3)
                             .scaleEffect(x: phase.isIdentity ? 1.0 : 0.7,
                                          y: phase.isIdentity ? 1.0 : 0.7)
