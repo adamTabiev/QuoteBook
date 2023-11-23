52b1ccd83a0f5abd3945de889f16a13a56bea0bb
Adam <tabievadam@gmail.com>
2023-11-23 20:40:24 +0100

    Add CustomScrollAnimationModifier

--- QuoteBook/Views/CommentView.swift
+++ QuoteBook/Views/CommentView.swift
@@ -21,15 +21,7 @@ struct CommentView: View {
                         comment: human.comment.randomElement() ?? ""
                     ))
                     .containerRelativeFrame(.horizontal, count: 1, spacing: 1.0)
-                    .scrollTransition { content, phase in
-                        content
-                            .rotation3DEffect(.degrees(phase.value * -30.0),
-                                              axis: (x: 0, y: 1, z: 0))
-                            .opacity(phase.isIdentity ? 1 : 0.2)
-                            .scaleEffect(x: phase.isIdentity ? 1.0 : 0.7,
-                                         y: phase.isIdentity ? 1.0 : 0.7)
-                            .offset(y: phase.isIdentity ? 0 : 50)
-                    }
+                    .modifier(CustomScrollAnimationModifier())
                 }
             }
             .scrollTargetLayout()
